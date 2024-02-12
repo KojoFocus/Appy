a326ff61ef0ee8a85086265851aa88fc5d3de4ff
KojoFocus <ernestobimpeh9@gmail.com>
2024-02-12 10:02:37 +0000

    Work on Appy Navbar

---
+++ js/main.js
@@ -0,0 +1,40 @@
+var firstName = "Michael"; //firstname is a word
+var lastName = "hamond"
+var age =67; // age is a number
+const boilingWaterTemperature = 100
+// let fullName= firstName + " " + lastName;
+let fullName = `${firstName} ${lastName}`;
+
+let a = 56;
+let b = 43;
+result= `${a} + ${b} = ${a+b}`
+console.log(fullName);
+console.log(result);
+let phone = {colour: "black", makeCall: null}
+
+if (age>=18) {console.log('welcome to XXX')}
+else {console.log('this page is not for you')}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
